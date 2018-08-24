3b387575063dc5ae25699e72d1606daf93eb01a7
Joyce Lin <joycelin79@gmail.com>
2018-08-23 17:59:24 -0700

    add license to description

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ function convertCollection(swaggerPath) {
 // call functions to handle conversion and update collection
 
 let collection_uid = "1559979-2de5594b-7153-4270-b280-451705f346f3";
-// let pathToSwaggerFile = './data/swagger.json';
-let pathToSwaggerFile = './data/petstore-expanded.json';
+let pathToSwaggerFile = './data/swagger.json';
+// let pathToSwaggerFile = './data/petstore-expanded.json';
 
 // retrieves postman collection based on uid
 // returns postman collection
